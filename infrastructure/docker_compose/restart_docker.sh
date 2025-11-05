3fea11f8fb47fb882b83fa704c5946c716b3a2c7
galamuz <muzychenko.galina@gmail.com>
2025-11-05 21:11:51 +0100

    add: run test pipeline

--- infrastructure/docker_compose/restart_docker.sh
+++ infrastructure/docker_compose/restart_docker.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-echo ">>> Остановить Docker Compose"
+echo ">>> Stop Docker Compose"
 docker compose down
 
-echo ">>> Docker pull все образы браузеров"
+echo ">>> Docker pull all browsers"
 
 # Путь до файла
 json_file="./config/browsers.json"
 
-# Проверяем, что jq установлен
 if ! command -v jq &> /dev/null; then
     echo "jq is not installed. Please install jq and try again."
     exit 1
 fi
 
-# Извлекаем все значения .image через jq
 images=$(jq -r '.. | objects | select(.image) | .image' "$json_file")
 
-# Пробегаем по каждому образу и выполняем docker pull
 for image in $images; do
     echo "Pulling $image..."
     docker pull "$image"
 done
 
-echo ">>> Запуск Docker Compose"
+echo ">>> Run Docker Compose"
 docker compose up -d
