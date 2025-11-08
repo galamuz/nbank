fc5e2fd3135bdca2c442bc4b03088bacce39361c
galamuz <muzychenko.galina@gmail.com>
2025-11-08 14:17:51 +0100

    add: allure steps reporter

--- src/test/java/ui/ChangeCustomerNameUITest.java
+++ src/test/java/ui/ChangeCustomerNameUITest.java
@@ -4,6 +4,7 @@ import api.generation.EntityGenerator;
 import api.models.CreateCustomerNameRequestModel;
 import common.annotation.UserSession;
 import common.storage.SessionStorage;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 import ui.pages.ProfilePage;
 import ui.pages.UserDashboardPage;
@@ -28,7 +29,7 @@ public class ChangeCustomerNameUITest extends BaseUITest {
     @Test
     @UserSession
     public void customerCanNotChangeNameForToShort() {
-//        String newShortName = RandomStringUtils.secure().nextAlphabetic(2).toLowerCase();
+        String newShortName = RandomStringUtils.secure().nextAlphabetic(2).toLowerCase();
 //        new UserDashboardPage().open().changeProfile().getPage(ProfilePage.class)
 //                .changeUserName(newShortName).checkAlertMessageAndAccept(UIAlerts.NAME_NOT_UPDATED);
 //
