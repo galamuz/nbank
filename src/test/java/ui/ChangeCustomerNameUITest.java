f868393148b218a1a76378ca42ae5afea1dcdc21
galamuz <muzychenko.galina@gmail.com>
2025-11-08 14:47:10 +0100

    add: allure steps reporter

--- src/test/java/ui/ChangeCustomerNameUITest.java
+++ src/test/java/ui/ChangeCustomerNameUITest.java
@@ -1,9 +1,6 @@
 package ui;
 
-import api.generation.EntityGenerator;
-import api.models.CreateCustomerNameRequestModel;
 import common.annotation.UserSession;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 
 public class ChangeCustomerNameUITest extends BaseUITest {
@@ -11,7 +8,7 @@ public class ChangeCustomerNameUITest extends BaseUITest {
     @UserSession
     public void customerCanChangeName() {
 
-        CreateCustomerNameRequestModel customerNameRequestModel = EntityGenerator.generate(CreateCustomerNameRequestModel.class);
+//        CreateCustomerNameRequestModel customerNameRequestModel = EntityGenerator.generate(CreateCustomerNameRequestModel.class);
 
 //        new UserDashboardPage().open().changeProfile().getPage(ProfilePage.class)
 //                .changeUserName(customerNameRequestModel.getName()).checkAlertMessageAndAccept(UIAlerts.NAME_UPDATED);
@@ -24,7 +21,7 @@ public class ChangeCustomerNameUITest extends BaseUITest {
     @Test
     @UserSession
     public void customerCanNotChangeNameForToShort() {
-        String newShortName = RandomStringUtils.secure().nextAlphabetic(2).toLowerCase();
+        //      String newShortName = RandomStringUtils.secure().nextAlphabetic(2).toLowerCase();
 //        new UserDashboardPage().open().changeProfile().getPage(ProfilePage.class)
 //                .changeUserName(newShortName).checkAlertMessageAndAccept(UIAlerts.NAME_NOT_UPDATED);
 //
