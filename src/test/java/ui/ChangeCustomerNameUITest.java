bf4427020f3b148bab9021b3cca406c9224f9867
galamuz <muzychenko.galina@gmail.com>
2025-11-08 14:06:58 +0100

    add: allure steps reporter

--- src/test/java/ui/ChangeCustomerNameUITest.java
+++ src/test/java/ui/ChangeCustomerNameUITest.java
@@ -1,19 +1,27 @@
 package ui;
 
+import api.generation.EntityGenerator;
+import api.models.CreateCustomerNameRequestModel;
 import common.annotation.UserSession;
+import common.storage.SessionStorage;
 import org.junit.jupiter.api.Test;
+import ui.pages.ProfilePage;
+import ui.pages.UserDashboardPage;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ChangeCustomerNameUITest extends BaseUITest {
     @Test
     @UserSession
     public void customerCanChangeName() {
-//        CreateCustomerNameRequestModel customerNameRequestModel = EntityGenerator.generate(CreateCustomerNameRequestModel.class);
-//
-//        new UserDashboardPage().open().changeProfile().getPage(ProfilePage.class)
-//                .changeUserName(customerNameRequestModel.getName()).checkAlertMessageAndAccept(UIAlerts.NAME_UPDATED);
-//
-//        assertThat(SessionStorage.getSteps().getProfile().getName())
-//                .isEqualTo(customerNameRequestModel.getName());
+
+        CreateCustomerNameRequestModel customerNameRequestModel = EntityGenerator.generate(CreateCustomerNameRequestModel.class);
+
+        new UserDashboardPage().open().changeProfile().getPage(ProfilePage.class)
+                .changeUserName(customerNameRequestModel.getName()).checkAlertMessageAndAccept(UIAlerts.NAME_UPDATED);
+
+        assertThat(SessionStorage.getSteps().getProfile().getName())
+                .isEqualTo(customerNameRequestModel.getName());
 
     }
 
